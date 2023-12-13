bfe8f1e66e4341bfe61730a7cfe1b63317d14521
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-12-13 21:30:01 +0600

    6. Constructor | Instance varia ble & method-ii

---
+++ 7. 6. Constructor | Instance varia ble & method-ii.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self, name,author):
+        self.name = name
+        self.authour = author
+        self.price = 0
+
+    def set_price(self, price):
+        self.price = price
+
+    def get_price(self):
+        return self.price
+    
+    def details(self):
+        print("Book Name:", self.name,
+              "\nAuthor:", self.authour, 
+              "\nprice:", self.price, "Tk.")
+
+b1 = Book("Rich dad poor dad", "Time Story")
+b1.details()
+b1.set_price(560)
+#print(b1.get_price())
+"""x=b1.get_price()
+print(x)"""
+b1.details()
