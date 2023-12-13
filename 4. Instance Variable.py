35317860c99aacdea75fe6d1b54cd91c89fb43d4
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-12-13 20:32:03 +0600

    Instance Variable

---
+++ 4. Instance Variable.py
@@ -0,0 +1,13 @@
+class House:
+    def __init__(self):
+        self.room = 4    #instance variable
+        self.window = 8   #instance variable
+        self.door = 6     #instance variable
+
+    def view(self):
+        """A method to display the house's features"""
+        print("Room:", self.room, ", Windows:", self.window,  ", Door:", self.door)
+
+h1 = House()
+print("House h1 has: ")
+h1.view()
