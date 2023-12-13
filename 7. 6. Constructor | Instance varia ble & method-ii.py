8092cfab465d244c59b34bdde584d2eb8225772a
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-12-13 21:33:12 +0600

    ok

--- 7. 6. Constructor | Instance varia ble & method-ii.py
+++ 7. 6. Constructor | Instance varia ble & method-ii.py
@@ -13,10 +13,10 @@ class Book:
     def details(self):
         print("Book Name:", self.name,
               "\nAuthor:", self.authour, 
-              "\nprice:", self.price, "Tk.")
+              "\nprice: $", self.price)
 
 b1 = Book("Rich dad poor dad", "Time Story")
-b1.details()
+b1.details()    # initial price because book price !=
 b1.set_price(560)
 #print(b1.get_price())
 """x=b1.get_price()
