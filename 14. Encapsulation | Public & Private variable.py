069a467471a545c9eab4a41d701590055a3f5088
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-12-15 00:25:44 +0600

    marge

--- 14. Encapsulation | Public & Private variable.py
+++ 14. Encapsulation | Public & Private variable.py
@@ -7,16 +7,39 @@ class Student:
     def details(self):           # instance method
         print("Name:", self.name, "ID:", self.__id)
 
-    def update_id(self, Id):
+
+    # def update_id(self, Id):
+    #     if(Id > 0):
+    #         self.__id = id
+    #     else:
+    #         print("Invalid ID, Try again please.")
+        
+        
+    def set_ID(self, Id):
         if(Id > 0):
             self.__id = id
+
         else:
             print("Invalid ID, Try again please.")
+
+
+    def  get_ID(self):
+        return self.__id
+    
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+    
 
 s1 = Student("Arafat", 23)
 s2 = Student("Mahmud", 19)
 
-s1.update_id(-999)
+# s1.update_id(-999)
+
+s2.set_ID(33)
+s1.set_name("Sidra")
 
 s1.details()
 s2.details()
