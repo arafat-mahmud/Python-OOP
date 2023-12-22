0c8a01f4cd7336c2ddc6636fcd2147f4eab6af6b
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-12-22 11:24:48 +0600

    20. Static Method-ii

---
+++ 20. Static Method-ii.py
@@ -0,0 +1,27 @@
+class Student:
+    university_name = "Abc"
+
+    def __init__(self, name, Id):
+        self.name = name
+        self.__id =Id
+
+    def details(self):
+        print("Name:", self.name, "ID:", self.__id, Student.university_name)
+
+    @classmethod
+    def update_university(cls, uni_name):
+        cls.university_name = uni_name
+
+    @staticmethod
+    def check_department(Id):
+        if Id[3:5] == "01" : print("CSE")
+        elif Id[3:5] == "41" : print("CS")
+
+Student.check_department("12345")
+
+# s1 = Student("Arafat", 23)
+# s2 = Student("Mahmud", 19)
+# s1.details()
+# s2.details()
+
+Student.check_department("67890")
