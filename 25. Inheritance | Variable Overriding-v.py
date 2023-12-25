20f3c473e95e1af2e4950e699aaba10dd3459c63
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-12-25 18:36:56 +0600

    Inheritance | Variable Overriding-v

--- 25. Inheritance | Variable Overriding-v.py
+++ 25. Inheritance | Variable Overriding-v.py
@@ -1,20 +1,20 @@
 class Animal:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.color = "white"
     
     def eat(self):
-        print(self.color,self.name,"is eating")
+        print(self.color, self.name, "is eating")
 
 class Dog(Animal):
 
-    def __init__(self,name,color):
+    def __init__(self, name, color):
         super().__init__(name)
         self.color = color
 
     def bark(self):
-        print(self.color,self.name,"is barking")
+        print(self.color, self.name, "is barking")
 
-d1 = Dog("Rover","Brown")
+d1 = Dog("Rover", "Brown")
 d1.eat()
 d1.bark()
